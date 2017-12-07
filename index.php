eb7509780d55c1e88b2de605575353423548d338
Saby69 <scarrezco@gmail.com>
2017-12-07 09:29:17 +0100

    Insertion du code dans index.php

---
+++ index.php
@@ -0,0 +1,176 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Création Site Internet - Lyon - France</title>
+        <meta name="description" content="Graphiste webdesigner freelance sur Lyon depuis 17 ans, j'assure la création graphique de tous vos outils & supports de communication visuelle.">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        <link rel="author" href="humans.txt">
+        <link rel="apple-touch-icon" href="tulipeicone.png">
+        <link rel="icon" type="image/png" href="tulipeicone.png" />
+        <!-- Place favicon.ico in the root directory -->
+
+    <link href="css/kaleis_content.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link href="css/kaleis_content.css" rel="stylesheet" type="text/css">
+
+        
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+       
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="container">
+          <header class="row">
+          <div id="equalheight"> 
+              <div class="col-sm-1 demo"> </div>
+                <div class="col-sm-3 demo"> 
+                    <img src="images/logo-final.png" alt="logo Kaleis" style="max-height:100%; max-width:100%;">
+                </div>
+                <div class="col-sm-5 demo"  style="padding-top: 20px;">  <p class="bold16ptbleu">Communication graphique sur mesure</p>
+                <p> </p>
+                 </div>
+                <div class="col-sm-3 demo" style="padding-top: 20px;">
+               
+                <p class="bold18ptblanc">06 52 73 10 73</p>
+                </div>
+                   
+        </div>
+          </header>
+          <div class="col-md-11" style="border-bottom:thin #CCC solid; padding-top:20px;"></div>
+          
+            <div class="col-md-5 col-lg-7"></div>
+            <div class="col-md-7 col-lg-5">
+            <?php include 'includes/menu.php';?>
+          </div>
+            
+            
+            <main>
+              <section class="row" id="intro">
+                  <div class="col-md-12">
+                      <div class="row">
+                        <div class="hidden-xs">
+                        <div class="col-md-1">
+                          </div>
+                          <div class="col-md-2">
+                          <img src="images/tulipe.png" alt="tulipe"> 
+                          </div>
+                        </div><!--fin idden xs -->
+                        <div class="col-md-6">
+                        <h3>Nos solutions Web et Print</h3>
+                        <p><strong>Spécialiste de la communication graphique depuis plus de 17 ans, nous vous accompagnons et conseillons à chaque étape de votre projet afin qu'il soit unique et qu'il vous ressemble ! <br>Nous nous adaptons à vos besoins !</strong></p>
+                        </div>
+                        <div class="col-md-3">
+                        </div>
+                        </div>
+                  </div>
+                </section>
+                                        
+                    <section class="row" id="solutions">
+                      
+                      <div class="col-md-4" id="web">
+                        <div class="cadre">
+                          <div class="row">
+                              <div class="col-md-12 fondbleu">
+                               <p class="blancgras14pt">Site internet <br \> Bannières Web</p>
+                                </div>
+                            </div>
+                            <div class="row">    
+                                <div class="col-md-12 fondblanc">
+                                <img src="images/montageweb.png" alt="creation site internet">
+                                </div>
+                            </div> 
+                            <div class="row">    
+                                <div class="col-md-12 fondblanc">
+                              <p span class="bold14gris">Nos solutions Web ! </p>
+                                </div>
+                             </div> 
+                        </div>
+                        </div>
+                      
+                        
+                        <div class="col-md-4" id="print">
+                        <div class="cadre">
+                          <div class="row">
+                              <div class="col-md-12 fondbleu">
+                                <p class="blancgras14pt">Cartes de visite <br \>Plaquettes...</p>
+                                </div>
+                            </div>
+                            <div class="row">    
+                                <div class="col-md-12 fondblanc">
+                               <img src="images/montageprint.png" alt="Creation cartes de visite">
+                                </div>
+                            </div> 
+                            <div class="row">    
+                                <div class="col-md-12 fondblanc">
+                                <p span class="bold14gris">Nos solutions print !</p>
+                                </div>
+                             </div>
+                              
+                        </div>
+                        </div>
+                      
+                        
+                        <div class="col-md-4" id="iv">
+                        <div class="cadre">
+                          <div class="row">
+                              <div class="col-md-12 fondbleu">
+                                <p class="blancgras14pt">Logo<br \>Charte graphique</p>
+                                </div>
+                            </div>
+                            <div class="row">    
+                                <div class="col-md-12 fondblanc">
+                                <img src="images/montageiv.png"  alt="Identité visuelle">
+                                </div>
+                            </div> 
+                            <div class="row">    
+                                <div class="col-md-12 fondblanc">
+                              <p span class="bold14gris">Nos solutions identité visuelle !</p>
+                                </div>
+                             </div> 
+                        </div>
+                        </div>
+                      
+                    
+        </section>
+                       
+        </main>
+            
+            <footer id="footer"> 
+              <div class="col-md-2"></div>
+              <div class="col-md-9"><?php include 'includes/footer.php';?>
+            </div>
+                <div class="col-md-1"></div>
+      </footer>
+            </div>
+            <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+                <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+                <script src="js/plugins.js"></script>
+                <script src="js/main.js"></script>
+        
+                <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+                <script>
+                    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+                    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+                    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+                    e.src='//www.google-analytics.com/analytics.js';
+                    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+                    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+                </script>
+        
+                 
+    </body>
+</html>
