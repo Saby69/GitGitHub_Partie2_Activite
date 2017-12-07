f8ace485717c0e8a9331babe9d3c178b581280f8
Saby69 <scarrezco@gmail.com>
2017-12-07 09:30:20 +0100

    Insertion du code dans portfolio.php

---
+++ portfolio.php
@@ -0,0 +1,287 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Création Site Internet - Lyon - France</title>
+        <meta name="description" content="Graphiste webdesigner freelance sur Lyon depuis 17 ans, j'assure la création graphique de tous vos outils & supports de communication visuelle.">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		
+        <link rel="author" href="humans.txt">
+        <link rel="apple-touch-icon" href="tulipeicone.png">
+        <link rel="icon" type="image/png" href="tulipeicone.png" />
+
+		<link href="css/kaleis_content.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/kaleis_content.css"  type="text/css">
+    	<link rel="stylesheet" href="css/nivo-slider.css"/>
+    	<link rel="stylesheet" href="css/portfolio.css"/>
+
+        
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+       
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="container">
+       	  <header class="row">
+          <div id="equalheight"> 
+          		<div class="col-sm-1 demo"> </div>
+               	<div class="col-sm-3 demo"> 
+                    <img src="images/logo-final.png" alt="logo Kaleis" style="max-height:70px; max-width:165px;">
+                </div>
+                <div class="col-sm-5 demo"  style="padding-top: 20px;">  <p class="bold16ptbleu">Communication graphique sur mesure</p>
+                <p> </p>
+                 </div>
+                <div class="col-sm-3 demo" style="padding-top: 20px;">
+               
+                <p class="bold18ptblanc">06 52 73 10 73</p>
+                </div>
+                   
+   		  </div>
+          </header>
+          <div class="col-md-11" style="border-bottom:thin #CCC solid; padding-top:20px;"></div>
+          
+            <div class="col-md-5 col-lg-7"></div>
+            <div class="col-md-7 col-lg-5">
+            	<?php include 'includes/menu.php';?>
+        	</div>
+            
+                    	<div class="row">
+                        	<div class="col-md-12" style="padding-bottom:30px; text-align:left;">
+                    	 	<h3>Quelques réalisations</h3>
+                     		</div>
+                      	</div>
+               	
+            <div class="container">
+            	<div class="row">
+                    <article class="col-xs-3 work" id="realisation-1-1">
+                        <img src="images/portfolio/lne-vignette.jpg" alt=""/><br>
+                        <div style="padding-top:10px;"><strong>Les Nuits Etoilées</strong><br>
+                        <em>Site Internet</em></div>
+                        <div class="work_detail">
+                            <hr/>
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <div class="work_slideshow stagger4">
+                                        <img src="images/portfolio/lne1-1.jpg"/>
+                                        <img src="images/portfolio/lne1-2.jpg"/>
+                                        <img src="images/portfolio/lne1-3.jpg"/>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <h2 class="stagger1">Les Nuits Etoilées</h2>
+                                    <p class="stagger2"><em>Chambres d'hôtes à Marrakech</em></p>
+                                    <p class="stagger3">Création du site internet<br><a href="http://www.lesnuitsetoilees.com">Visiter le site</a></p>
+                                </div>
+                            </div>
+                            <hr/>
+                        </div>
+                    </article>
+                    <article class="col-xs-3 work" id="realisation-1-2">
+                        <img src="images/portfolio/losfor-vignette.jpg" alt=""/><br>
+                        <div style="padding-top:10px;"><strong>Losfor</strong><br>
+                        <em>Site Internet</em></div>
+                        <div class="work_detail">
+                            <hr/>
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <div class="work_slideshow stagger4">
+                                        <img src="images/portfolio/losfor2-1.jpg"/>
+                                        <img src="images/portfolio/losfor2-2.jpg"/>
+                                        <img src="images/portfolio/losfor2-3.jpg"/>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <h2 class="stagger1">Losfor</h2>
+                                    <p class="stagger2"><em>Site Internet</em></p>
+                                    <p class="stagger3">Création du site internet<br><a href="http://www.losfor.com">Visiter le site</a></p>
+                                </div>
+                            </div>
+                            <hr/>
+                        </div>
+                    </article>
+                    <article class="col-xs-3 work" id="realisation-1-3">
+                        <img src="images/portfolio/formaposte-vignette.jpg" alt=""/><br>
+                        <div style="padding-top:10px;"><strong>Formaposte</strong><br>
+                        <em>Print - Digital</em></div>
+                        <div class="work_detail">
+                            <hr/>
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <div class="work_slideshow stagger4">
+                                        <img src="images/portfolio/formaposte1-1.jpg"/>
+                                        <img src="images/portfolio/formaposte1-2.jpg"/>
+                                        <img src="images/portfolio/formaposte1-3.jpg"/>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <h2 class="stagger1">Formaposte</h2>
+                                    <p class="stagger2"><em>Print - Digital</em></p>
+                                    <p class="stagger3">Bannières, Annonces presse...</p>
+                                </div>
+                            </div>
+                            <hr/>
+                        </div>
+                    </article>
+                    <article class="col-xs-3 work" id="realisation-1-4">
+                        <img src="images/portfolio/pyx4-vignette.jpg" alt=""/><br>
+                        <div style="padding-top:10px;"><strong>Pyx4</strong><br>
+                        <em>Print - Digital</em></div>
+                        <div class="work_detail">
+                            <hr/>
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <div class="work_slideshow stagger4">
+                                        <img src="images/portfolio/pyx41-1.jpg"/>
+                                        <img src="images/portfolio/pyx41-2.jpg"/>
+                                        <img src="images/portfolio/pyx41-3.jpg"/>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <h2 class="stagger1">Pyx4</h2>
+                                    <p class="stagger2"><em>Print - Digital</em></p>
+                                    <p class="stagger3">Cartes de visite, flyers, kakemono, bannières...</p>
+                                </div>
+                            </div>
+                            <hr/>
+                        </div>
+                    </article>
+                </div>
+                <div class="row-detail"></div>
+                <div class="row">
+                    <article class="col-xs-3 work" id="realisation-2-1">
+                        <img src="images/portfolio/llec-vignette.jpg" alt=""/><br>
+                        <div style="padding-top:10px;"><strong>Les Lycéens en Coeur du Rhône</strong><br>
+                        <em>Print</em></div>
+                        <div class="work_detail">
+                            <hr/>
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <div class="work_slideshow stagger4">
+                                        <img src="images/portfolio/llec1-1.jpg"/>
+                                        <img src="images/portfolio/llec1-2.jpg"/>
+                                        <img src="images/portfolio/llec1-1.jpg"/>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <h2 class="stagger1">Les Lycéens en Coeur du Rhône</h2>
+                                    <p class="stagger2"><em>Print</em></p>
+                                    <p class="stagger3">Flyers, Affiche...</p>
+                                </div>
+                            </div>
+                            <hr/>
+                        </div>
+                    </article>
+                    <article class="col-xs-3 work" id="realisation-2-2">
+                        <img src="images/portfolio/globalliance-vignette.jpg" alt=""/><br>
+                        <div style="padding-top:10px;"><strong>Globalliance</strong><br>
+                        <em>Print - Digital</em></div>
+                        <div class="work_detail">
+                            <hr/>
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <div class="work_slideshow stagger4">
+                                        <img src="images/portfolio/globalliance1-1.jpg"/>
+                                        <img src="images/portfolio/globalliance1-2.jpg"/>
+                                        <img src="images/portfolio/globalliance1-3.jpg"/>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <h2 class="stagger1">Globalliance</h2>
+                                    <p class="stagger2"><em>Print - Digital</em></p>
+                                    <p class="stagger3">Cartes de visite, flyers, bannières, chemises à rabats...</p>
+                                </div>
+                            </div>
+                            <hr/>
+                        </div>
+                    </article>
+                    <article class="col-xs-3 work" id="realisation-2-3">
+                        <img src="images/portfolio/xlcium-vignette.jpg" alt=""/><br>
+                        <div style="padding-top:10px;"><strong>XLcium</strong><br>
+                        <em>Print - Digital</em></div>
+                        <div class="work_detail">
+                            <hr/>
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <div class="work_slideshow stagger4">
+                                        <img src="images/portfolio/xlcium1-1.jpg"/>
+                                        <img src="images/portfolio/xlcium1-2.jpg"/>
+                                        <img src="images/portfolio/xlcium1-1.jpg"/>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <h2 class="stagger1">XLcium</h2>
+                                    <p class="stagger2"><em>Print - Digital</em></p>
+                                    <p class="stagger3">Flyers, bannières, mail, site web...</p>
+                                </div>
+                            </div>
+                            <hr/>
+                        </div>
+                    </article>
+                    <article class="col-xs-3 work" id="realisation-2-4">
+                        <img src="images/portfolio/divers-vignette.jpg" alt=""/><br>
+                        <div style="padding-top:10px;"><strong>Divers</strong><br>
+                        <em>Print - Digital</em></div>
+                        <div class="work_detail">
+                            <hr/>
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <div class="work_slideshow stagger4">
+                                        <img src="images/portfolio/divers1-1.jpg"/>
+                                        <img src="images/portfolio/divers1-2.jpg"/>
+                                        <img src="images/portfolio/divers1-3.jpg"/>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <h2 class="stagger1">Divers</h2>
+                                    <p class="stagger2"><em>Print - Digital</em></p>
+                                    <p class="stagger3">Cartes de visite, flyers, kakemono, bannières, logo, newsletter, bannières animées...</p>
+                                </div>
+                            </div>
+                            <hr/>
+                        </div>
+                    </article>
+               </div>
+    			<div class="row-detail"></div>
+	    	</div>
+            
+            <footer id="footer"> 
+            	<div class="col-md-2"></div>
+            	<div class="col-md-9"><?php include 'includes/footer.php';?>
+        		</div>
+                <div class="col-md-1"></div>
+			</footer>
+            </div>
+				
+                <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+                <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+                <script src="js/plugins.js"></script>
+                <script src="js/main.js"></script>
+        
+                <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+                <script>
+                    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+                    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+                    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+                    e.src='//www.google-analytics.com/analytics.js';
+                    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+                    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+                </script>
+            <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+			<script src="js/jquery.nivo.slider.pack.js"></script>
+			<script src="js/portfolio.js"></script>
+                 
+    </body>
+</html>
